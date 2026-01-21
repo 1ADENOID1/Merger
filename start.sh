82c1aeec4ef23969dd587f54022b1372e7a152f4
1ADENOID1 <sashazaytsev2003@mail.ru>
2026-01-21 14:41:07 +0500

    Исправление sh файла

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 set -a
-source config.env
+source ./config.env
 set +a
 
 java -jar target/JSONpars-1.0-SNAPSHOT-jar-with-dependencies.jar Parser $UPDATED_RES_DIR $CURRENT_RES_DIR $BACKUP_DIR $CREATE_BACKUP_SUBDIR $CREATE_CHANGELOG $FILE_ENCODING
-pause
