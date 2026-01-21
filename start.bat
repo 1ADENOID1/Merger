2a35c9fac914b8633b995c776764a011356d6042
1ADENOID1 <sashazaytsev2003@mail.ru>
2026-01-21 14:09:07 +0500

    Добавление .env файла с конфигураций и sh файла запуска для Linux

--- start.bat
+++ start.bat
@@ -1,9 +1,6 @@
-set DISTR_DIR=C:\\Users\\azaytsev\\Desktop\\serv\\config
-set USER_DIR=C:\\Users\\azaytsev\\Desktop\\serv1\\config
-set BACKUP_DIR=C:\\Users\\azaytsev\\Desktop\\userBackup
-set CREATE_BACKUP_SUBDIR=true
-set CREATE_CHANGELOG=true
-set FILE_ENCODING=UTF-8
+@echo off
 
-java -jar target/JSONpars-1.0-SNAPSHOT-jar-with-dependencies.jar Parser %DISTR_DIR% %USER_DIR% %BACKUP_DIR% %CREATE_BACKUP_SUBDIR% %CREATE_CHANGELOG% %FILE_ENCODING%
+for /f "usebackq delims=" %%i in ("config.env") do (set "%%i")
+
+java -jar target/JSONpars-1.0-SNAPSHOT-jar-with-dependencies.jar Parser %UPDATED_RES_DIR% %CURRENT_RES_DIR% %BACKUP_DIR% %CREATE_BACKUP_SUBDIR% %CREATE_CHANGELOG% %FILE_ENCODING%
 pause
