0ab1358c94c4106f940b4527f6120df1600d90d9
1ADENOID1 <sashazaytsev2003@mail.ru>
2026-01-26 15:35:03 +0500

    Удалён sleep для отладки из start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,3 @@ source ./config.env
 set +a
 
 java -jar target/AppletMerger-1.0-jar-with-dependencies.jar $UPDATED_RES_DIR $CURRENT_RES_DIR $BACKUP_DIR $CREATE_BACKUP_SUBDIR $CREATE_CHANGELOG $FILE_ENCODING
-sleep 1000
